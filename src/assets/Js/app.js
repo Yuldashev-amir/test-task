b658485f860849c9ff83acd0f5d50f2fb8f7a8fd
Amir <amir-uldashev@mail.ru>
2022-05-16 00:07:19 +0600

    commit develop 5

--- src/assets/Js/app.js
+++ src/assets/Js/app.js
@@ -1,20 +1,8 @@
-$('.multiple-items').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 1
-  });
-  $('.center').slick({
-    centerMode: true,
-    slidesToShow: 3,
-  });
-let slider = $("#Header__slider");
-
-slider.slick({
-  infinite: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  fade: true,
-  arrows: false,
+$('.slick-item').slick({
+  infinite: true,
   dots: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
 });
+
+$('.slick-item').slick();
